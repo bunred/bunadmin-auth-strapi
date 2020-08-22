a6f1bff67d85ecd26e20f6a26b0a1dee6bf19d36
Chris533 <ganchengs@gmail.com>
2020-08-22 22:16:21 +0800

    Update package (rename, add props), add tsconfig

---
+++ index.ts
@@ -0,0 +1,8 @@
+import security from "./utils/security"
+import initData from "./utils/initData"
+export * from "./utils/types"
+
+import users from "./users"
+import signIn from "./sign-in"
+
+export { security, initData, signIn, users }
